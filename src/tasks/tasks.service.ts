30d7956ceb678ab873bea8fda6537712ae3efb2b
pedrogodoy <pedrogh.godoy12@gmail.com>
2020-07-23 13:33:42 -0400

    filter task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
-import { CreateTaskDto } from './create-task.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,26 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+
+        let tasks = this.getAllTasks();
+
+        if(status) {
+            //filtra os itens que tiverem o mesmo status
+            tasks = tasks.filter(task => task.status === status);
+        }
+
+        if(search)  {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) ||
+                task.description.includes(search),
+            );
+        }
+
+        return tasks;
     }
 
     getTaskById(id: string): Task {
