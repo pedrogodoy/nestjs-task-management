632eca990f4bf7fec38917bfa011d0a7a43e3d81
pedrogodoy <pedrogh.godoy12@gmail.com>
2020-07-27 14:31:44 -0400

    getting task with typeorm

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskRepository } from './task.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
 
 @Injectable()
 export class TasksService {
-
+    constructor(
+        @InjectRepository(TaskRepository)
+        private taskRepository: TaskRepository
+    ) {}
     // getAllTasks(): Task[] {
     //     return this.tasks;
     // }
@@ -29,16 +35,16 @@ export class TasksService {
     //     return tasks;
     // }
 
-    // getTaskById(id: string): Task {
-    //     const found = this.tasks.find(task => task.id === id);
+    async getTaskById(id: number): Promise<Task> {
+        const found = await this.taskRepository.findOne(id);
 
-    //     //Error handler for task not found
-    //     if(!found) {
-    //         throw new NotFoundException(`Task with Id ${id} not found`);
-    //     }
+        //Error handler for task not found
+        if(!found) {
+            throw new NotFoundException(`Task with Id ${id} not found`);
+        }
 
-    //     return found;
-    // }
+        return found;
+    }
 
     // createTask(createTaskDto: CreateTaskDto): Task {
     //     //Extract title and description from DTO
