79f3f86a39156d9cfdf8ca2b4accee0e626eb0bd
pedrogodoy <pedrogh.godoy12@gmail.com>
2020-07-27 16:44:12 -0400

    get tasks with filter - typeORM

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -10,6 +10,14 @@ export class TaskRepository extends Repository<Task> {
         const { status, search } = filterDto;
         const query = this.createQueryBuilder('task');
 
+        if(status) {
+            query.andWhere('task.status = :status', { status })
+        }
+
+        if(search) {
+            query.andWhere('task.status LIKE :search OR task.description LIKE :search', { search: `%${search}%` });
+        }
+
         const tasks = await query.getMany();
         return tasks;
     }
